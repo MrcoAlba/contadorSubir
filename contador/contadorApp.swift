dca62f7288f7b1c3a04223e5e482407a6037e4a5
MrcoAlba <mrcoalba@gmail.com>
2024-05-23 01:35:59 -0500

    Initial Commit

---
+++ contador/contadorApp.swift
@@ -0,0 +1,17 @@
+//
+//  contadorApp.swift
+//  contador
+//
+//  Created by Mrco Alba on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct contadorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
